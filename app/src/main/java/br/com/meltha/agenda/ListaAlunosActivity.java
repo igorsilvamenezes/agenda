b726118bdd2f433f08c13956332564ebb96601e5
Igor Menezes <igorsilvamenezes@gmail.com>
2019-09-24 23:29:04 -0300

    Commit inicial - Estrutura inicial do projeto

---
+++ app/src/main/java/br/com/meltha/agenda/ListaAlunosActivity.java
@@ -0,0 +1,14 @@
+package br.com.meltha.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+    }
+}
