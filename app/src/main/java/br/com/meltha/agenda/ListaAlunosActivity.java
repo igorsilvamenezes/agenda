063900a17a274dd5d15730dbd273d1bb4ec0f841
Igor Menezes <igorsilvamenezes@gmail.com>
2019-09-25 23:23:30 -0300

    Melhorando a lista de Alunos, utilizando ListView com array de Strings

--- app/src/main/java/br/com/meltha/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/meltha/agenda/ListaAlunosActivity.java
@@ -3,6 +3,11 @@ package br.com.meltha.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos =  {"Igor", "Sandra", "Malyssa", "Maryana"};
+        ListView listaAlunos = findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 }
