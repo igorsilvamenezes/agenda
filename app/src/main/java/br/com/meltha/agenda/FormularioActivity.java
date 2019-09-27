937fd849c074d367b51bdcf762b62ebaec0c526f
Igor Menezes <igorsilvamenezes@gmail.com>
2019-09-26 22:57:01 -0300

    Tratamento do clique do botão salvar

--- app/src/main/java/br/com/meltha/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/meltha/agenda/FormularioActivity.java
@@ -3,6 +3,9 @@ package br.com.meltha.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar = findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Cadastro Salvo", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
